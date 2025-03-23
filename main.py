4aba333f2598155949de46fc7cc4120f6cd011b9
nsulonen <niko.sulonen@proton.me>
2025-03-23 18:44:42 +0200

    create group for asteroid objects

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame import SurfaceType
 from pygame.time import Clock
 from player import Player
+from asteroid import Asteroid
 
 from constants import *
 
@@ -13,8 +14,10 @@ def main() -> None:
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
 
     x: int = SCREEN_WIDTH / 2
     y: int = SCREEN_HEIGHT / 2
