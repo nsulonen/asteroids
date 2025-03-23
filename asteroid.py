754434d07b6130bae63b990a5e96a895a2697468
nsulonen <niko.sulonen@proton.me>
2025-03-23 19:09:27 +0200

    fixes to asteroid class

--- asteroid.py
+++ asteroid.py
@@ -3,14 +3,12 @@ from circleshape import CircleShape
 
 
 class Asteroid(CircleShape):
-    def __init__(self, x: int, y: int, radius: int) -> None:
-        super().__init__(self, x, y)
+    def __init__(self, x, y, radius) -> None:
+        super().__init__(x, y, radius)
         self.radius = radius
-        self.position: tuple = (x, y)
-        self.velocity = super().velocity
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        self.position += self.velocity * dt
+        self.position += (self.velocity * dt)
