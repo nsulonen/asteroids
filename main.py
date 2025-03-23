8708f02d9306ad676a2d12493e86a97a82fbefae
nsulonen <niko.sulonen@proton.me>
2025-03-23 16:39:25 +0200

    set screen refresh rate to 60

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
+from pygame import SurfaceType
+from pygame.time import Clock
+
 from constants import *
 
 def main() -> None:
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen: SurfaceType = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock: Clock = pygame.time.Clock()
+    dt: float = 0.0
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +18,9 @@ def main() -> None:
         screen.fill(color=(0,0,0))
 
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
